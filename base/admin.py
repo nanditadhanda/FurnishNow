96ec33d1a6bcc34ff6703a458dc5d8f52300b131
Nandita Dhanda <dhandandita@gmail.com>
2021-09-15 06:17:03 +0800

    django admin fixes

--- base/admin.py
+++ base/admin.py
@@ -13,11 +13,26 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('_id', 'orderDate', 'totalPrice',
+                    'orderStatus')
+    # readonly fields
+    readonly_fields = ('user', 'orderDate')
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'name', 'qty',
+                    'price')
+
+    # readonly fields
+    readonly_fields = ('order',)
+
+
 # Register your models here.
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Review)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(ShippingAddress)
 # admin.site.register(Report)
