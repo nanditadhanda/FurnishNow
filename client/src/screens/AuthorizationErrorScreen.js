a86419be83a9d93482c5907593d3b07288123086
nanditadhanda <dhandanandita@gmail.com>
2021-12-12 07:24:34 +0800

    Unautharized Access Page desig

--- client/src/screens/AuthorizationErrorScreen.js
+++ client/src/screens/AuthorizationErrorScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Message from '../components/Message'
+import { Container, Button } from 'react-bootstrap'
+import {IoMdArrowBack} from 'react-icons/io'
 
 const AuthorizationErrorScreen = () => {
     return (
-        <div>
-            You are not authorized to access this page.
-            - if not logged in, log in to continue
-            - go back to home button            
-        </div>
+        <Container className='py-5 align-center'>
+            <h1 className='mb-4'>Access Denied</h1>
+            <Message variant="warning">
+                You are not authorized to access this page.       
+            </Message>
+            <Link to ="/">
+                <Button variant="outline-success">
+                    <IoMdArrowBack/> Go To Homepage</Button>
+            </Link>
+        </Container>       
     )
 }
 
