fd9b0755187e271ea21e4b41a901250f9bc6647c
nanditadhanda <dhandanandita@gmail.com>
2021-12-15 08:01:03 +0800

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Nav} from 'react-bootstrap'
 
 const Footer = () => {
     return (
         <footer className="py-3">
-            <Container>
+            <Container fluid>
                 <Row>
-                    <Col className="text-center">&copy; 2021 FurnishNow</Col>
+                    <Col xs="5" className="text-left">&copy; 2021 FurnishNow</Col>
+                    <Col className="d-flex justify-content-end">
+                        <Nav className="text-secondary">     
+                            <Nav.Link className="py-0" href="/about">About</Nav.Link>
+                            <Nav.Link className="py-0" href="/contact">Contact</Nav.Link>
+                            <Nav.Link className="py-0" href="/privacy">Privacy</Nav.Link>
+                            <Nav.Link className="py-0" href="/tnc">T&amp;C</Nav.Link>
+                        </Nav>
+                    </Col>             
                 </Row>
             </Container>
         </footer>
