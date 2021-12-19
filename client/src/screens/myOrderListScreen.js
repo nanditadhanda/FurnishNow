b53d2564f0031a9b5fc08e950d62089592d8b91d
nanditadhanda <dhandanandita@gmail.com>
2021-12-19 17:54:51 +0800

    My Order List Update
    - add ordercard component

--- client/src/screens/myOrderListScreen.js
+++ client/src/screens/myOrderListScreen.js
@@ -14,6 +14,7 @@ import {Container,Row, Col, Card, Table, ListGroup, Button, Image} from 'react-b
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Paginate from '../components/Paginate'
+import OrderCard from '../components/OrderCard'
 
 const MyOrderListScreen = ({history}) => {
 
@@ -63,67 +64,7 @@ const MyOrderListScreen = ({history}) => {
                     <Message variant="danger">Error: {error}</Message>
                 ) : (
                     orders.map(order => (
-                        <Card key={order._id} className="my-4 shadow-sm"> 
-                            <Card.Header className="d-flex justify-content-between">
-                                <small className="">Order ID: {order._id}</small>   
-                                <small>Status: <span className="text-success">Order {order.orderStatus}</span></small>
-                            </Card.Header>
-
-                            <Row>
-                                <Col className="">
-                                    <ListGroup variant="flush" className="pe-1 ps-2 py-2">
-                                        <ListGroup.Item>
-                                            <h6>Order Items: </h6>
-                                        </ListGroup.Item>
-                                    {/* Map out items in order */}
-                                    {order.orderItems.map((item, index) => (
-                                        <ListGroup.Item key="index">
-                                            <Row>
-                                                {/* Product Image */}
-                                                <Col md={2} xs={3}>
-                                                    <Image src={item.image} alt={item.name} fluid rounded></Image>
-                                                </Col>
-                                                {/* Product Name */}
-                                                <Col>
-                                                    {item.name}
-                                                    <br/>    
-                                                    <span className="text-secondary">{item.qty} x RM {item.price}</span>
-                                                </Col>
-                                                {/* Quantity and Price */}
-                                                <Col md={4} className="text-success text-right">
-                                                   RM {(parseFloat(item.qty * item.price)).toFixed(2)}
-                                                </Col>
-                                            </Row>
-                                        </ListGroup.Item>
-                                    ))}
-                                </ListGroup>
-                                </Col>
-
-                                 <Col md={4} className="border-start px-5 py-3">
-                                    <Table>
-                                        <tr>
-                                            <td><h6 className="p-0">Order Date:</h6></td>
-                                            <td className="text-right">{order.orderDate.substring(0, 10)}</td>
-                                        </tr>
-                                        <tr>
-                                            <td><h6 className="p-0"> Order Total:</h6></td>
-                                            <td className="text-right">RM {order.totalPrice}</td>
-                                        </tr>
-                                        <tr >
-                                            <td colSpan="2">
-                                                <LinkContainer to={`/order/${order._id}`}>
-                                                    <div className="d-grid px-0 my-3">
-                                                        <Button variant="outline-success"  type='button'>View Order</Button>
-                                                    </div>
-                                                </LinkContainer>
-                                                
-                                            </td>
-                                        </tr>
-                                    </Table>
-                                </Col>
-                                
-                            </Row>  
-                        </Card>
+                        <OrderCard order={order}  key={order._id}/>                   
                         
                     ))
                 )}
